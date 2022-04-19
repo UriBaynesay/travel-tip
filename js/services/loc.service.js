8486b0b82c38f564738139ce629699545361bf8a
Uri Baynesay <uri552@gmail.com>
2022-04-19 16:43:40 +0300

    added createLoc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,41 @@
 export const locService = {
-    getLocs
+  createLoc,
+  getLocs,
+};
+
+import { storageService } from "./storage.service.js";
+import { utils } from "./utils.js";
+
+const LOCATION_KEY = "locationDB";
+
+const locs = storageService.load(LOCATION_KEY)
+  ? storageService.load(LOCATION_KEY)
+  : [
+      { name: "Greatplace", lat: 32.047104, lng: 34.832384 },
+      { name: "Neveragain", lat: 32.047201, lng: 34.832581 },
+    ];
+
+// createLoc("home", 32.047104, 34.832384, "")
+
+function createLoc(name, lat, lng, weather) {
+    const loc={
+        id: utils.makeId(),
+        name,
+        lat,
+        lng,
+        weather,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      }
+      locs.push(loc);
+      storageService.save(LOCATION_KEY,locs)
+  return Promise.resolve(loc);
 }
-
-
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs);
+    }, 2000);
+  });
 }
-
-
